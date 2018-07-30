1997b7030ea8d406bf6bfddc3643eb3a9ceb3146
yutinglaitw <ytlai.tw@gmail.com>
2018-07-30 23:01:13 +0800

    add db

--- db_connector.py
+++ db_connector.py
@@ -12,7 +12,7 @@ def mysql_connector(sql_command):
         with connection.cursor() as cursor:
             # Create a new record
             cursor.execute(sql_command)
-            connection.commit()
+            # connection.commit()
             result = cursor.fetchall()
     except:
         connection.rollback()
@@ -21,3 +21,41 @@ def mysql_connector(sql_command):
 
     return result
 
+'''
+MSSQL connector
+'''
+def mssql_connector(sql_command):
+    import pymssql
+    connection = pymysql.connect(host='localhost',
+                                 user='user',
+                                 password='passwd',
+                                 database='db')
+
+    try:
+        with connection.cursor() as cursor:
+            # Create a new record
+            cursor.execute(sql_command)
+            # connection.commit()
+            result = cursor.fetchall()
+    except:
+        connection.rollback()
+    finally:
+        connection.close()
+
+    return result
+
+
+'''
+MongoDB connector
+'''
+def mongodb_connector(host, port, database, collection):
+    import pymongo
+
+    client = pymongo.MongoClient(host=host, port=port)
+    db = client[database]
+    coll = db[collection].find()
+
+    result = coll.find()
+    # coll.find().limit(10).skip(20)
+
+    return result
